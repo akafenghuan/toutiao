fc13683a9cf4f61686aa30753bf093abc615dcf1
fenghuan <GongZhongmeng@126.com>
2021-10-08 15:12:43 +0800

    登录注册-优化封装token本地存储

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { setItem, getItem } from '../utils/storage'
 
 Vue.use(Vuex)
-
+const USER_KEY = 'toutiao-user'
 export default new Vuex.Store({
   state: {
-    user: JSON.parse(window.localStorage.getItem('user'))
+    // user: JSON.parse(window.localStorage.getItem('user'))
+    user: getItem(USER_KEY)
     // 当前登录用户的登录状态（token等数据）
   },
   mutations: {
@@ -14,7 +16,8 @@ export default new Vuex.Store({
 
       // 为了防止页面刷新数据丢失，需要将数据放到本地存储中
       // 这一步操作是为了持久化数据
-      window.localStorage.setItem('user', JSON.stringify(state.user))
+      // window.localStorage.setItem('user', JSON.stringify(state.user))
+      setItem(USER_KEY, state.user)
     }
   },
   actions: {
